fdefae9fb145bc889fd325ddac66053c4ae2d884
happyeliia <elia.zontaa@gmail.com>
2022-09-09 10:28:42 +0200

    working on google auth app

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Google Sign In'),
     );
   }
 }
@@ -104,6 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
+        
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
