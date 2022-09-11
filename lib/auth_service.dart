96d23f25c4956b0bb9ab18ad2c8e1e315b3484f0
happyeliia <elia.zontaa@gmail.com>
2022-09-11 12:17:39 +0200

    google-auth app working

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+
+import 'home_page.dart';
+import 'login_page.dart';
 
 
 class AuthService {
@@ -14,5 +19,25 @@ class AuthService {
         }
       }
     );
+  }
+   signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn(
+        scopes: <String>["email"]).signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+  signOut(){
+    FirebaseAuth.instance.signOut();
   }
 }
